72ee23c18e18f7d4abb5c3276b9d141931793bb3
MAYUR <mayur.nic@gmail.com>
2019-07-11 19:09:25 +0530

    Reverse Queue

---
+++ src/queue/ReverseQueueUsingStack.java
@@ -0,0 +1,26 @@
+package queue;
+
+import stack.StackUsingLinkedList;
+
+public class ReverseQueueUsingStack {
+
+    StackUsingLinkedList stackUsingLinkedList;
+    LinkedQueue linkedQueue;
+    public static void main(String args[])
+    {
+        ReverseQueueUsingStack reverseQueueUsingStack=new ReverseQueueUsingStack();
+        reverseQueueUsingStack.linkedQueue=new LinkedQueue();
+        reverseQueueUsingStack.stackUsingLinkedList=new StackUsingLinkedList();
+        for(int i=0; i<10; i++) {
+            reverseQueueUsingStack.linkedQueue.Enqueue(i);
+        }
+        reverseQueueUsingStack.linkedQueue.print();
+        while (reverseQueueUsingStack.linkedQueue.front!=null) {
+            reverseQueueUsingStack.stackUsingLinkedList.push(reverseQueueUsingStack.linkedQueue.front.getData());
+            reverseQueueUsingStack.linkedQueue.DeEnqueue();
+        }
+
+        reverseQueueUsingStack.stackUsingLinkedList.print();
+
+    }
+}
