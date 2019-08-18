dcbf910933fd1d7c285e9d2bdf1f70520e1bcf38
MAYUR <mayur.nic@gmail.com>
2019-08-18 18:07:44 +0530

    BreadthFirstTraversal

--- src/graph/BreadthFirstSearch.java
+++ src/graph/BreadthFirstSearch.java
@@ -1,12 +1,43 @@
 package graph;
 
+import queue.LinkedQueueObject;
 import trees.BinaryTree;
 import trees.BinaryTreeObject;
 
 public class BreadthFirstSearch {
+
+    public static void breadthFistSearch(GraphLinkedListObject graphLinkedListObjects )
+    {
+        LinkedQueueObject queue=new LinkedQueueObject();
+        queue.Enqueue(graphLinkedListObjects);
+        graphLinkedListObjects.visited=true;
+
+        while(!queue.isEmpty())
+        {
+
+            GraphLinkedListObject vertex=(GraphLinkedListObject) queue.Dequeue();
+            System.out.println(vertex.data);
+            for(GraphLinkedListObject temp:vertex.getAdjacentNodeList())
+            {
+                if(temp.visited==false) {
+                    queue.Enqueue(temp);
+                    temp.visited = true;
+                }
+            }
+        }
+    }
     public static void main(String args[])
     {
-        //BinaryTreeObject
+        Graph graph=new Graph(5);
+        // System.out.println(graph.linkedListObjects.length);
+        graph.linkedListObjects[0].addEdge(graph.linkedListObjects[1]);
+        graph.linkedListObjects[0].addEdge(graph.linkedListObjects[4]);
+        graph.linkedListObjects[0].addEdge(graph.linkedListObjects[3]);
+        graph.linkedListObjects[1].addEdge(graph.linkedListObjects[4]);
+        graph.linkedListObjects[1].addEdge(graph.linkedListObjects[4]);
+        graph.linkedListObjects[3].addEdge(graph.linkedListObjects[2]);
+        breadthFistSearch(graph.linkedListObjects[1]);
     }
+
 
 }
--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -18,37 +18,4 @@ public class Graph {
     }
 
 
-    public static void breadthFistSearch(GraphLinkedListObject graphLinkedListObjects )
-    {
-        LinkedQueueObject queue=new LinkedQueueObject();
-        queue.Enqueue(graphLinkedListObjects);
-        graphLinkedListObjects.visited=true;
-
-        while(!queue.isEmpty())
-        {
-
-            GraphLinkedListObject vertex=(GraphLinkedListObject) queue.Dequeue();
-            System.out.println(vertex.data);
-            for(GraphLinkedListObject temp:vertex.getAdjacentNodeList())
-            {
-                if(temp.visited==false) {
-                    queue.Enqueue(temp);
-                    temp.visited = true;
-                }
-            }
-            }
-    }
-    public static void main(String args[])
-    {
-        Graph graph=new Graph(5);
-       // System.out.println(graph.linkedListObjects.length);
-        graph.linkedListObjects[0].addEdge(graph.linkedListObjects[1]);
-        graph.linkedListObjects[0].addEdge(graph.linkedListObjects[4]);
-        graph.linkedListObjects[0].addEdge(graph.linkedListObjects[3]);
-        graph.linkedListObjects[1].addEdge(graph.linkedListObjects[4]);
-        graph.linkedListObjects[1].addEdge(graph.linkedListObjects[4]);
-        graph.linkedListObjects[3].addEdge(graph.linkedListObjects[2]);
-        breadthFistSearch(graph.linkedListObjects[1]);
-    }
-
 }
